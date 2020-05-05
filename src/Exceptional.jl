2e7983449efb34540366b3f7dec43700e2567b8d
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-05-05 20:18:10 +0200

    invoke_restart hack

--- src/Exceptional.jl
+++ src/Exceptional.jl
@@ -21,15 +21,22 @@ function block(func)
     end
 end
 
-function return_from(name, value = nothing)
-    throw([name, value])
+return_from(name, value = nothing) = throw([name, value])
+
+function available_restart(name)
+    println(name)
+    false
 end
 
-available_restart(name) = nothing
+invoke_restart(name, args...) = name, args
 
-invoke_restart(name, args...) = nothing
-
-restart_bind(func, restarts...) = nothing
+function restart_bind(func, restarts...)
+    try
+        func()
+    catch e
+        rethrow([e, restarts])
+    end
+end
 
 error(exception::Exception) = throw(exception)
 
@@ -37,11 +44,21 @@ function handler_bind(func, handlers...)
     try
         func()
     catch e
-        if isa(e, Array)
-            rethrow(e)
-        end
         for handler in handlers
             exception_type, handler_function = handler
+            if isa(e, Array)
+                if exception_type == typeof(e[1])
+                    name, args = handler_function(e[1])
+                    for restart_action in e[2]
+                        restart_name, restart_function = restart_action
+                        if name == restart_name
+                            return restart_function(args...)
+                        end
+                    end
+                else
+                    rethrow(e)
+                end
+            end
             if exception_type == typeof(e)
                 handler_function(e)
                 rethrow(e)
@@ -49,5 +66,40 @@ function handler_bind(func, handlers...)
         end
     end
 end
+
+
+reciprocal(value) =
+    restart_bind(
+        :return_zero => () -> 0,
+        :return_value => identity,
+        :retry_using => reciprocal,
+    ) do
+        value == 0 ? error(DivisionByZero()) : 1 / value
+    end
+
+println(handler_bind(DivisionByZero => (c) -> invoke_restart(:return_zero)) do
+    reciprocal(0)
+end == 0)
+
+println(handler_bind(DivisionByZero => (c) -> invoke_restart(:return_value, 123)) do
+    reciprocal(0)
+end == 123)
+
+println(handler_bind(DivisionByZero => (c) -> invoke_restart(:retry_using, 10)) do
+    reciprocal(0)
+end == 0.1)
+
+# println(
+#     handler_bind(
+#         DivisionByZero => (c) -> for restart in (:return_one, :return_zero, :die_horribly)
+#             if available_restart(restart)
+#                 invoke_restart(restart)
+#             end
+#         end,
+#     ) do
+#         reciprocal(0)
+#     end,
+# )
+
 
 end
--- src/RestartsInJulia.jl
+++
@@ -1,1 +0,0 @@
-include("Exceptional.jl")
